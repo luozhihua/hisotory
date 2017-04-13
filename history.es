6aa69219d573cc0a245e45fedfae3274d8061c59
Colin Luo <mail@luozhihua.com>
2017-04-13 11:41:50 +0800

    suported back\forward\go\pushState\replaceState and other fetures are fallback to origin history apis.

--- history.es
+++ history.es
@@ -1,19 +1,119 @@
 import Event from "respondent-emitter";
 
-function OriginHistory() {}
-OriginHistory.prototype = window.history;
+class OriginHistory extends Event {
+	custructor() {
+		let history = window.history
 
+		Object.keys(history.__proto__).forEach((methods) => {
+			if (typeof history[methods] === 'function') {
+				this[methods] = history[methods].bind(history)
+			}
+		});
+	}
+}
+
+let store = [];
+let current = 0;
 export default class History extends OriginHistory {
 
-    constructor() {
-    	super();
+    constructor(options) {
+    	super()
 
-    	debugger;
-	    this.store = [];
-	    this.current = 0;
-	    this.event = new Event();
-	    this.back = function() {
+    	this.options = {
+    		base: '/',
+    		hashbang: false,
+    		...options
+    	}
+    }
 
+    back() {
+    	let from = store[current];
+    	let to = store[current-1] || null;
+
+    	if (this.emit('beforeNavigate', from, to) !== false) {
+    		super.back()
+    		current -= 1
+    	}
+    	this.emit('afterNavigate')
+    }
+
+    forward() {
+    	let from = store[current];
+    	let to = store[current+1] || null;
+
+    	if (this.emit('beforeNavigate', from, to) !== false) {
+    		super.forward()
+    		current += 1
+    	}
+    	this.emit('afterNavigate')
+    }
+
+    go(number=0) {
+    	number = parseInt(number)
+
+    	if (number === 0) {
+    		return;
+    	} else {
+	    	let toIndex = current + number
+	    	let from = store[current]
+	    	let to = store[toIndex]
+
+	    	if (this.emit('beforeNavigate', from, to) !== false) {
+	    		super.go(number)
+	    		current = toIndex
+	    		this.emit('afterNavigate')
+	    	}
 	    }
     }
+
+    pushState(state, title, url) {
+	    url = this.formatPath(url)
+
+    	if (this.emit('beforeNavigate') !== false) {
+	    	store.length = current;
+
+	    	store.push({
+	    		state: state,
+	    		title: title,
+	    		url: url
+	    	})
+	    	super.pushState(state, title, url)
+	    	current = store.length
+	    	this.emit('afterNavigate')
+	    }
+    }
+
+    replaceState(state, title, url) {
+	    url = this.formatPath(url)
+
+    	if (this.emit('beforeNavigate') !== false) {
+	    	store.length = current;
+
+	    	store[current] = {
+	    		state: state,
+	    		title: title,
+	    		url: url
+	    	}
+	    	super.replaceState(state, title, url)
+	    	this.emit('afterNavigate')
+	    }
+    }
+
+    getHistoryIndexByUrl(url) {
+    	let index;
+    	store.forEach((state, i) => {
+    		if (state.url === url) {
+    			index = i
+    			return false
+    		}
+    	})
+    }
+
+    formatPath(path) {
+    	path = path.replace(this.options.base, '') || '/'
+
+    	return (this.options.hashbang ? '#!' : '') + path
+    }
+
+
 }
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-				value: true
+   value: true
 });
 
-var _respondentEmitter = require("respondent-emitter");
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _respondentEmitter = require('respondent-emitter');
 
 var _respondentEmitter2 = _interopRequireDefault(_respondentEmitter);
 
@@ -16,26 +22,152 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-function OriginHistory() {}
-OriginHistory.prototype = window.history;
+var OriginHistory = function (_Event) {
+   _inherits(OriginHistory, _Event);
+
+   function OriginHistory() {
+      _classCallCheck(this, OriginHistory);
+
+      return _possibleConstructorReturn(this, (OriginHistory.__proto__ || Object.getPrototypeOf(OriginHistory)).apply(this, arguments));
+   }
+
+   _createClass(OriginHistory, [{
+      key: 'custructor',
+      value: function custructor() {
+         var _this2 = this;
+
+         var history = window.history;
+
+         Object.keys(history.__proto__).forEach(function (methods) {
+            if (typeof history[methods] === 'function') {
+               _this2[methods] = history[methods].bind(history);
+            }
+         });
+      }
+   }]);
+
+   return OriginHistory;
+}(_respondentEmitter2.default);
+
+var store = [];
+var current = 0;
 
 var History = function (_OriginHistory) {
-				_inherits(History, _OriginHistory);
+   _inherits(History, _OriginHistory);
 
-				function History() {
-								_classCallCheck(this, History);
+   function History(options) {
+      _classCallCheck(this, History);
 
-								var _this = _possibleConstructorReturn(this, (History.__proto__ || Object.getPrototypeOf(History)).call(this));
+      var _this3 = _possibleConstructorReturn(this, (History.__proto__ || Object.getPrototypeOf(History)).call(this));
 
-								debugger;
-								_this.store = [];
-								_this.current = 0;
-								_this.event = new _respondentEmitter2.default();
-								_this.back = function () {};
-								return _this;
-				}
+      _this3.options = _extends({
+         base: '/',
+         hashbang: false
+      }, options);
+      return _this3;
+   }
 
-				return History;
+   _createClass(History, [{
+      key: 'back',
+      value: function back() {
+         var from = store[current];
+         var to = store[current - 1] || null;
+
+         if (this.emit('beforeNavigate', from, to) !== false) {
+            _get(History.prototype.__proto__ || Object.getPrototypeOf(History.prototype), 'back', this).call(this);
+            current -= 1;
+         }
+         this.emit('afterNavigate');
+      }
+   }, {
+      key: 'forward',
+      value: function forward() {
+         var from = store[current];
+         var to = store[current + 1] || null;
+
+         if (this.emit('beforeNavigate', from, to) !== false) {
+            _get(History.prototype.__proto__ || Object.getPrototypeOf(History.prototype), 'forward', this).call(this);
+            current += 1;
+         }
+         this.emit('afterNavigate');
+      }
+   }, {
+      key: 'go',
+      value: function go() {
+         var number = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+
+         number = parseInt(number);
+
+         if (number === 0) {
+            return;
+         } else {
+            var toIndex = current + number;
+            var from = store[current];
+            var to = store[toIndex];
+
+            if (this.emit('beforeNavigate', from, to) !== false) {
+               _get(History.prototype.__proto__ || Object.getPrototypeOf(History.prototype), 'go', this).call(this, number);
+               current = toIndex;
+               this.emit('afterNavigate');
+            }
+         }
+      }
+   }, {
+      key: 'pushState',
+      value: function pushState(state, title, url) {
+         url = this.formatPath(url);
+
+         if (this.emit('beforeNavigate') !== false) {
+            store.length = current;
+
+            store.push({
+               state: state,
+               title: title,
+               url: url
+            });
+            _get(History.prototype.__proto__ || Object.getPrototypeOf(History.prototype), 'pushState', this).call(this, state, title, url);
+            current = store.length;
+            this.emit('afterNavigate');
+         }
+      }
+   }, {
+      key: 'replaceState',
+      value: function replaceState(state, title, url) {
+         url = this.formatPath(url);
+
+         if (this.emit('beforeNavigate') !== false) {
+            store.length = current;
+
+            store[current] = {
+               state: state,
+               title: title,
+               url: url
+            };
+            _get(History.prototype.__proto__ || Object.getPrototypeOf(History.prototype), 'replaceState', this).call(this, state, title, url);
+            this.emit('afterNavigate');
+         }
+      }
+   }, {
+      key: 'getHistoryIndexByUrl',
+      value: function getHistoryIndexByUrl(url) {
+         var index = void 0;
+         store.forEach(function (state, i) {
+            if (state.url === url) {
+               index = i;
+               return false;
+            }
+         });
+      }
+   }, {
+      key: 'formatPath',
+      value: function formatPath(path) {
+         path = path.replace(this.options.base, '') || '/';
+
+         return (this.options.hashbang ? '#!' : '') + path;
+      }
+   }]);
+
+   return History;
 }(OriginHistory);
 
 exports.default = History;
